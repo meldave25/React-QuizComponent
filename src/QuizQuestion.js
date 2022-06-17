e894541100be01ce3f5f2db28d541dedf119f530
Melvin Puthanangadi <meldave25@gmail.com>
2022-06-17 13:36:37 -0500

    Last module completed

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -37,7 +37,7 @@ class QuizQuestion extends Component {
               </ul>
               </section>
               {this.state.incorrectAnswer ? <p className="error">Sorry, that';s not right</p> : null}
-              {console.log(this.state.incorrectAnswer)}
+             
       </main>
 
     
